d0b8557547af6e506527f9e333adf75c404facda
hantd <hantd@jointem.com>
2016-07-20 14:01:44 +0800

    add Dept.java File

---
+++ src/com/jointe/com/Dept.java
@@ -0,0 +1,48 @@
+package com.jointe.com;
+
+/**
+ * @Title: Dept
+ * @Description:
+ * @Author: htd
+ * @Date: 2016-7-20
+ * @Version: v1.0.0
+ * @Copyright: 2016 www.jointem.com Inc. All rights reserved.
+ * @Update: 1. Create File
+ */
+public class Dept {
+
+	private Integer id;
+
+	private String name;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
