2bcc7cc4db1638723424b6421dc5aa38d2806196
hantd <hantd@jointem.com>
2016-07-20 13:58:13 +0800

    add　User.java File

---
+++ src/com/jointe/com/User.java
@@ -0,0 +1,83 @@
+package com.jointe.com;
+
+import java.util.Date;
+
+/**
+ * @Title: User
+ * @Description:
+ * @Author: htd
+ * @Date: 2016-7-20
+ * @Version: v1.0.0
+ * @Copyright: 2016 www.jointem.com Inc. All rights reserved.
+ * @Update: 1. Create File
+ */
+public class User {
+	private Integer id;
+
+	private String userName;
+
+	private String password;
+
+	private Date lastLogin;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the lastLogin
+	 */
+	public Date getLastLogin() {
+		return lastLogin;
+	}
+
+	/**
+	 * @param lastLogin
+	 *            the lastLogin to set
+	 */
+	public void setLastLogin(Date lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+}
